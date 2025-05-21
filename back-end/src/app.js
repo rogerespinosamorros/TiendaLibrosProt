c7264c37fb40c7db41069eb1f231aa42613026de
Roger <rogerespinosamorros@gmail.com>
2025-05-21 11:05:38 +0200

    admin-profile(common.js)

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -1,9 +1,18 @@
 const express = require('express');
 require('dotenv').config();
+const mongoose = require('mongoose');
 
 const app = express();
 
 const port = process.env.PORT || 3000;
+const mongoURI = process.env.MONGO_URI || 'mongodb://localhost:27017/tiendaLibrosProt';
+
+mongoose.connect(mongoURI, {})
+    .then(() => {
+        console.log('MongoDB connected');
+    })
+    .catch(error => console.error(`MongoDB connection error: ${error}`));
+    
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
---
+++ back-end/src/utils/common.js
@@ -0,0 +1,30 @@
+const bcrypt = require('bcryptjs');
+const User = require('../models/User');
+
+const createAdminAcc = async () => {
+    try{
+        const email = process.env.ADMIN_EMAIL;
+        const password = process.env.ADMIN_PASSWORD;
+        const firstName = process.env.ADMIN_FIRST_NAME;
+        const lastName = process.env.ADMIN_LAST_NAME;
+        const ifExistsAdmin = await User.findOne({email: email});
+        if (ifExistsAdmin) {
+            console.log('Admin account already exists');
+            return;
+        };
+        const hashPassword = await bcrypt.hash(password, 10);
+        const admin = new User({
+            email: email,
+            firstName: firstName,
+            lastName: lastName,
+            role: 'admin',
+            password: hashPassword,
+        });
+        await admin.save();
+        console.log('Admin account created successfully');
+    } catch (error) {
+        console.error(`Error creating admin account: ${error}`);
+    }
+};
+
+module.exports = {createAdminAcc};

d5817a312edcbf58c27cb7a43d6bb351ff7584cc
Roger <rogerespinosamorros@gmail.com>
2025-05-21 09:12:44 +0200

    env document-inicio user.js

---
+++ back-end/src/models/User.js
@@ -0,0 +1,29 @@
+const moongose = require('mongoose');
+const { Schema } = moongose;
+
+new Schema({
+    email:{
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password:{
+        type: String,
+        required: true,
+    },
+    firstName:{
+        type: String,
+        required: true,
+    },
+    lastName:{
+        type: String,
+        required: true,
+    },
+    role:{
+        type: String,
+        required: true,
+        // 'admin' or 'user'
+        enum: ['admin', 'user'],
+        default: 'user',
+    }
+})   

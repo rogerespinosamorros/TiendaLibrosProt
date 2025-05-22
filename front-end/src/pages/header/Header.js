65c7bf20db2b4542ec921aec57d557717d453c71
Roger <rogerespinosamorros@gmail.com>
2025-05-22 10:55:46 +0200

    version-header-copilot

--- front-end/src/pages/header/Header.js
+++ front-end/src/pages/header/Header.js
@@ -1,7 +1,37 @@
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
+import Button from '@mui/material/Button';
+import MenuIcon from '@mui/icons-material/Menu';
+import { Link } from 'react-router-dom';
+
+
 export default function Header() {
   return (
-    
-      <h1>Header Works</h1>
+    <>
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{ mr: 2 }}
+              >
+                <MenuIcon />
+              </IconButton>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              Tienda de Libros
+            </Typography>
+            <Button component={Link} to="/login" color="inherit">Login</Button>
+            <Button component={Link} to="/register" color="inherit">Register</Button>
+          </Toolbar>
+        </AppBar>
+      </Box>
+    </>
     
   );
 }

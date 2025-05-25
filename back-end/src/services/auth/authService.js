721525cadfb80332eaa7f48371a3b4626d73a458
Roger <rogerespinosamorros@gmail.com>
2025-05-25 16:58:13 +0200

    login-user & createUser

---
+++ back-end/src/services/auth/authService.js
@@ -0,0 +1,59 @@
+const bcrypt = require('bcryptjs');
+const User = require('../../models/User');
+const jwt = require('jsonwebtoken');
+
+
+//Create user account
+const createUser = async (userData) => {
+    try {
+        const email = userData.email;
+        const firstName = userData.firstName;
+        const lastName = userData.lastName;
+        const password = userData.password;
+
+        const ifExistsUser = await User.findOne({ email: email });
+        if (ifExistsUser) {
+            throw new Error('User already exists');
+        };
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const user = new User({
+            email: email,
+            firstName: firstName,
+            lastName: lastName,
+            role: 'user',
+            password: hashedPassword,
+        });
+        await user.save();
+        return user
+    } catch (error) {
+        console.error(`Error creating user: ${error}`);
+    }
+};
+
+
+const loginUser = async (userData) => {
+    try {
+        const email = userData.email;
+        const password = userData.password;
+
+        const ifExistsUser = await User.findOne({ email: email });
+        if (!ifExistsUser) {
+            throw new Error('User not exists');
+        };
+
+        const itsCorrectPassword = await bcrypt.compare(password, ifExistsUser.password);
+        if (!itsCorrectPassword) {
+            throw new Error('Incorrect password');
+        };
+        const token = jwt.sign({ id: ifExistsUser._id, role: ifExistsUser.role },
+            process.env.JWT_SECRET_KEY,
+            { expiresIn: '1d' });
+        return token;
+
+    } catch (error) {
+        console.error(`Error creating user: ${error}`);
+    }
+};
+
+module.exports = { createUser, loginUser };

f2b6279a18bffa0da63d6b9ac270c1d6044d63fb
Roger <rogerespinosamorros@gmail.com>
2025-05-26 12:21:16 +0200

    gestion tokens en commonjs (consejoCopilot)

---
+++ front-end/src/utils/common.js
@@ -0,0 +1,39 @@
+import Cookies from 'js-cookie';
+import {jwtDecode} from 'jwt-decode';
+
+const TOKEN = 'token';
+
+export const saveToken = (token) => {
+    Cookies.set(TOKEN, token);
+};
+
+export const getToken = () => {
+    return Cookies.get(TOKEN) || null;
+};
+
+export const decodeToken = () => {
+    const token = getToken();
+    if (!token) {
+        return null;
+    }
+    try {
+        return jwtDecode(token);
+    } catch (error) {
+        console.error('Error decoding token:', error);
+        return null;
+    }
+}
+
+export const isAdminLoggedIn = () => {
+    const decodedToken = decodeToken();
+    return decodedToken?.role === 'admin';
+}
+
+export const isUserLoggedIn = () => {
+    const decodedToken = decodeToken();
+    return decodedToken?.role === 'user';
+}
+
+export const removeToken = () => {
+    Cookies.remove(TOKEN);
+}

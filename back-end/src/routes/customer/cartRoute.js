0bfef7a165a9f24350e5e86ca5abde025ba85766
Roger <rogerespinosamorros@gmail.com>
2025-06-02 11:24:44 +0200

    retoques archivos cart API

--- back-end/src/routes/customer/cartRoute.js
+++ back-end/src/routes/customer/cartRoute.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const { authenticateJWT, authorizeRole } = require('../../middlewares/authMiddlewares');
-const { postBookToCart } = require('../../controllers/customer/cartController');
+const { postBookToCart, getCartByUser } = require('../../controllers/customer/cartController');
 
 router.post('/:bookId', authenticateJWT, authorizeRole('customer'), postBookToCart);
+
+router.get('/', authenticateJWT, authorizeRole('customer'), getCartByUser);
 
 module.exports = router;
--- back-end/src/services/customer/cartService.js
+++ back-end/src/services/customer/cartService.js
@@ -5,10 +5,10 @@ const CartItem = require('../../models/cartItem');
 
 
 const addBookToCart = async (userId, bookId) => {
-    const activeOrder =  await Order.findOne({ user: userId, orderStatus: 'Pending' }).populate('cartItem');
+    const activeOrder = await Order.findOne({ user: userId, orderStatus: 'Pending' }).populate('cartItem');
     if (!activeOrder) return { status: 404, data: 'No active order found for this user.' };
 
-    if (activeOrder.cartItem.some(item => item.book.toString() === bookId)) 
+    if (activeOrder.cartItem.some(item => item.book.toString() === bookId))
         return { status: 409, data: 'This book is already in the cart.' };
 
     const [book, user] = await Promise.all([
@@ -16,8 +16,8 @@ const addBookToCart = async (userId, bookId) => {
         User.findById(userId)
     ]);
 
-    if (!book || !user) return { status: 404, data: 'Book or user not fiund'};
-    
+    if (!book || !user) return { status: 404, data: 'Book or user not fiund' };
+
     const savedCartItem = await new CartItem({
         order: activeOrder,
         user,
@@ -30,10 +30,22 @@ const addBookToCart = async (userId, bookId) => {
     activeOrder.cartItem.push(savedCartItem);
     await activeOrder.save();
     return { status: 201, data: 'Book added to cart successfully' };
-       
+
+};
+
+const fetchCartByUser = async (userId) => {
+    const activeOrder = await Order.findOne({ user: userId, orderStatus: 'Pending' })
+        .populate({
+            path: 'cartItem',
+            populate: { path: 'book' }
+        });
+    if (!activeOrder) return { status: 404, data: 'Active order not found' };
+
+    return { status: 200, data: activeOrder };
+
 };
 
 
 module.exports = {
-    addBookToCart
+    addBookToCart, fetchCartByUser
 };
